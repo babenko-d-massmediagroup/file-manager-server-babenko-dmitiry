32ae691b4ef9a6b7ea17bc96e26d158f8d53238a
babenko-d-massmediagroup <mmg.babenko.d@gmail.com>
2022-01-14 14:58:19 +0200

    feature: statistic

--- src/statistic/statistic.controller.ts
+++ src/statistic/statistic.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { StatisticService } from './statistic.service';
 
 @Controller('statistic')
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('')
+  async receiveFullStatistic(@Req() req) {
+    return this.statisticService.receiveFullStatistic(req.user.id);
+  }
 }
